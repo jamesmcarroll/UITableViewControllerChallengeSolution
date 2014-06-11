53648ec994cf3f9af51b4c90db532790b56857e6
James Carroll <jamescarroll@Jamess-MacBook-Pro.local>
2014-06-11 11:38:35 -0500

    second commit

--- UITableViewController Challenge Solution/UITableViewControllerClass.h
+++ UITableViewController Challenge Solution/UITableViewControllerClass.h
@@ -1,5 +1,5 @@
 //
-//  CCViewController.h
+//  UITableViewControllerClass.h
 //  UITableViewController Challenge Solution
 //
 //  Created by James Carroll on 6/11/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface UITableViewControllerClass : UITableViewController
 
 @end
